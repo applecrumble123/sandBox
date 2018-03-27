fac886433ad6effe4679d5d3929b4bf6bd80a987
applecrumble123 <30610249+applecrumble123@users.noreply.github.com>
2018-03-27 10:38:29 +0800

    Counting characters in a string

--- oddName.py
+++ oddName.py
@@ -1,1 +1,13 @@
-print ("Hello World")
+import string
+
+def letter_count(a_str):
+    """Return the count of letters in a_str"""
+    count = 0
+    for char in a_str:
+        if char.lower() in string.ascii_lowercase:
+            count += 1
+    return count
+
+f = letter_count("hello")
+print (f)
+
